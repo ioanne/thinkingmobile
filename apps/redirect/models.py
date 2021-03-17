cd433ea71d9f385af56e35bf2fe83c4ce47a82d7
ioanne <juanignaciobonini@hotmail.com>
2021-03-17 16:54:20 -0300

    Add redirect model. Add memcached property.

--- apps/redirect/models.py
+++ apps/redirect/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.core.cache import caches
 
-# Create your models here.
+from apps.core.models import BoostedModel
+
+
+class Redirect(BoostedModel):
+    key = models.CharField(max_length=254)
+    url = models.URLField()
+    active = models.BooleanField(default=True)
+
+    @property
+    def cache(self):
+        return caches['default']
