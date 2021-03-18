dc43bb423f6b39f4368306ff9e66891fd5c0a3e4
ioanne <juanignaciobonini@hotmail.com>
2021-03-17 21:30:20 -0300

    Add new logic to model.

--- apps/redirect/models.py
+++ apps/redirect/models.py
@@ -1,7 +1,16 @@
+import logging
+
 from django.db import models
 from django.core.cache import caches
 
 from apps.core.models import BoostedModel
+
+logger = logging.getLogger('thinkingmobile')
+cache = caches['default']
+
+class RedirectManager(models.Manager):
+    def get_queryset(self):
+        return super(RedirectManager, self).get_queryset().filter(active=True)
 
 
 class Redirect(BoostedModel):
@@ -9,6 +18,38 @@ class Redirect(BoostedModel):
     url = models.URLField()
     active = models.BooleanField(default=True)
 
-    @property
-    def cache(self):
-        return caches['default']
+    objects = RedirectManager()
+    objects_all = models.Manager()
+
+    @classmethod
+    def get_redirect(cls, key):
+        redirects = cls.get_active_from_cache()
+        redirect = redirects.get(key)
+        if redirect:
+            redirect = {'key':key, 'url': redirect}
+        return redirect
+
+    @staticmethod
+    def set_active_to_cache():
+        """Method to cache all active redirects."""
+        cache_key = 'all_active_redirects'
+        data = dict(Redirect.objects.values_list('key', 'url'))
+        cache.set(cache_key, data, 60 * 60 * 24)
+        logger.info('All active redirects are cached.')
+        return data
+
+    @classmethod
+    def get_active_from_cache(cls):
+        """Method to obtain all active redirects from cache."""
+        cache_key = 'all_active_redirects'
+        data = cache.get(cache_key)
+        if not data:
+            # If it is not stored, it is cached and returned
+            data = cls.set_active_to_cache()
+            logger.info('All active redirects are cached.')
+        logger.info('Cached data is returned.')
+        return data
+
+
+    def __str__(self):
+        return f'{self.id} - {self.url} - {self.key}'
