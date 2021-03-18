a27c435b7e852556ffb5728a77c0a48f2761d24c
ioanne <juanignaciobonini@hotmail.com>
2021-03-17 21:27:12 -0300

    The logger is configured.

--- thinkingmobile/settings.py
+++ thinkingmobile/settings.py
@@ -104,6 +104,30 @@ CACHES = {
     }
 }
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'standard': {
+            'format': "[%(asctime)s] [%(levelname)s] %(message)s",
+            'datefmt': "%d/%b/%Y %H:%M:%S"
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard',
+        }
+    },
+    'loggers': {
+        'thinkingmobile': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        }
+    },
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
