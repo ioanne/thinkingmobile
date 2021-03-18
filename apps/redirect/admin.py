3c1d7b2580a6185ce43db08744a622cf84dd1d53
ioanne <juanignaciobonini@hotmail.com>
2021-03-17 21:27:37 -0300

    Add model to Admin.

--- apps/redirect/admin.py
+++ apps/redirect/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.redirect.models import Redirect
+
+
+@admin.register(Redirect)
+class RedirectAdmin(admin.ModelAdmin):
+    search_fields = ['id', 'key', 'url']
+    list_display = ('id', 'key', 'url',)
